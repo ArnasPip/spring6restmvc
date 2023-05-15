33ec30c8f5f587cc405326f71abea978fd1b14d5
arnaspip <arnas.pipinis@stud.vilniustech.lt>
2023-05-15 22:03:27 +0300

    Adding paging and sorting

--- src/main/java/chevo/springrestmvc/spring6mvc/service/BeerServiceImpl.java
+++ src/main/java/chevo/springrestmvc/spring6mvc/service/BeerServiceImpl.java
@@ -3,6 +3,8 @@ package chevo.springrestmvc.spring6mvc.service;
 import chevo.springrestmvc.spring6mvc.model.BeerDTO;
 import chevo.springrestmvc.spring6mvc.model.BeerStyle;
 import org.slf4j.Logger;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -61,8 +63,8 @@ public class BeerServiceImpl implements BeerService {
     }
 
     @Override
-    public List<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory){
-        return new ArrayList<>(beerMap.values());
+    public Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber, Integer pageSize){
+        return new PageImpl<>(new ArrayList<>(beerMap.values()));
     }
 
     @Override
